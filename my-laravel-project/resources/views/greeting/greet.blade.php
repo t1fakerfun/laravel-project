504d6dfe715aa6ba0c768b3886c446d251b4ac4d
Almalinux <almalinux@localhost.localdomain>
2025-05-13 17:13:42 +0900

    05-01 greeting1

---
+++ my-laravel-project/resources/views/greeting/greet.blade.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="ja">
+    <head>
+        <meta charset="UTF-8">
+        <title>挨拶</title>
+    </head>
+<body>
+    <h1>こんにちは!</h1>
+    <p>皆さん、こんにちは</p>
+</body>
+</html>
--- my-laravel-project/routes/web.php
+++ my-laravel-project/routes/web.php
@@ -1,7 +1,13 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\GreetingController;
 
 Route::get('/', function () {
     return view('welcome');
 });
+Route::get('greeting',[GreetingController::class,'greet']);
+
+
+
+
